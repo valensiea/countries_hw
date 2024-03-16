091f96ab64ab72d06519fb77e1fcd5f003dcb9e5
Pattanawadee Chawraina <pattanawadee.26.30@gmail.com>
2024-03-16 23:22:35 +0700

    pop up

--- lib/countries.dart
+++ lib/countries.dart
@@ -3,12 +3,20 @@ class Country {
   final String? capital;
   final int? population;
   final String? flag;
+  final String? currency;
+  final String? phone;
+  final String? abbreviation;
+  final String? emblem;
 
   Country({
     required this.name,
     required this.capital,
     required this.population,
     required this.flag,
+    required this.currency,
+    required this.phone,
+    required this.abbreviation,
+    required this.emblem,
   });
 
   factory Country.fromJson(Map<String, dynamic> json) {
@@ -17,6 +25,10 @@ class Country {
       capital: json['capital'],
       population: json['population'],
       flag: json['media'] != null ? json['media']['flag'] : null,
+      currency: json['currency'],
+      phone: json['phone'],
+      abbreviation: json['abbreviation'],
+      emblem: json['media'] != null ? json['media']['emblem'] : null,
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,8 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   List<Country>? _countries;
+  List<Country>? _filteredCountries;
+  final TextEditingController _searchController = TextEditingController();
 
   @override
   void initState() {
@@ -39,22 +41,53 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void _getCountries() async {
-    try {
-      var dio = Dio(BaseOptions(responseType: ResponseType.plain));
-      var response =
-          await dio.get('https://api.sampleapis.com/countries/countries');
-      List<dynamic> list = jsonDecode(response.data);
+    // try {
+    //   var dio = Dio(BaseOptions(responseType: ResponseType.plain));
+    //   var response =
+    //       await dio.get('https://api.sampleapis.com/countries/countries');
+    //   List<dynamic> list = jsonDecode(response.data);
+    //   setState(() {
+    //     _countries = list.map((country) => Country.fromJson(country)).toList();
+    //     _countries!.sort((a, b) => a.name!.compareTo(b.name!));
+    //   });
+    // } catch (e) {
+    //   print('Error fetching countries: $e');
+    //   // Handle the error gracefully, e.g., show a message to the user
+    //   setState(() {
+    //     _countries = []; // Clear the countries list or set it to null
+    //   });
+    // }
+    var dio = Dio(BaseOptions(responseType: ResponseType.plain));
+    var response =
+        await dio.get('https://api.sampleapis.com/countries/countries');
+
+    List list = jsonDecode(response.data);
+
+    setState(() {
+      _countries = list.map((country) => Country.fromJson(country)).toList();
+      _filteredCountries = _countries;
+      _countries!.sort((a, b) => a.name!.compareTo(b.name!));
+    });
+  }
+
+  void _filterCountries(String query) {
+    if (query.isEmpty) {
       setState(() {
-        _countries = list.map((country) => Country.fromJson(country)).toList();
-        _countries!.sort((a, b) => a.name!.compareTo(b.name!));
+        _filteredCountries = _countries;
       });
-    } catch (e) {
-      print('Error fetching countries: $e');
-      // Handle the error gracefully, e.g., show a message to the user
+    } else {
       setState(() {
-        _countries = []; // Clear the countries list or set it to null
+        _filteredCountries = _countries!
+            .where((country) =>
+                country.name!.toLowerCase().contains(query.toLowerCase()))
+            .toList();
       });
     }
+  }
+
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
   }
 
   @override
@@ -63,35 +96,130 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: const Text("Countries of the World"),
       ),
-      body: _buildCountryList(),
+      // body: _buildCountryList(),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _searchController,
+              decoration: const InputDecoration(
+                labelText: 'Search',
+                suffixIcon: Icon(Icons.search),
+              ),
+            ),
+          ),
+          Expanded(
+            child: _filteredCountries == null
+                ? SizedBox.shrink()
+                : ListView.builder(
+                    itemCount: _filteredCountries!.length,
+                    itemBuilder: (context, index) {
+                      var country = _filteredCountries![index];
+                      return ListTile(
+                        title: Text(country.name ?? ''),
+                        subtitle: Text(country.abbreviation ?? ''),
+                        trailing: country.flag!.isNotEmpty
+                            ? Image.network(
+                                country.flag!,
+                                errorBuilder: (context, error, stackTrace) {
+                                  return Icon(Icons.error);
+                                },
+                              )
+                            : const SizedBox(
+                                width: 50,
+                                height: 50,
+                              ),
+                        onTap: () {
+                          _showCountryDetails(
+                              country.capital as String,
+                              country.population as int,
+                              country.currency as String,
+                              country.phone as String,
+                              country.flag as String,
+                              country.emblem as String);
+                        },
+                      );
+                    },
+                  ),
+          )
+        ],
+      ),
     );
   }
 
-  Widget _buildCountryList() {
-    if (_countries == null) {
-      return const Center(child: CircularProgressIndicator());
-    } else if (_countries!.isEmpty) {
-      return const Center(child: Text('No countries found'));
-    } else {
-      return ListView.builder(
-        itemCount: _countries!.length,
-        itemBuilder: (context, index) {
-          var country = _countries![index];
+  // Widget _buildCountryList() {
+  //   if (_countries == null) {
+  //     return const Center(child: CircularProgressIndicator());
+  //   } else if (_countries!.isEmpty) {
+  //     return const Center(child: Text('No countries found'));
+  //   } else {
+  //     return ListView.builder(
+  //       itemCount: _countries!.length,
+  //       itemBuilder: (context, index) {
+  //         var country = _countries![index];
 
-          return ListTile(
-            title: Text(country.name ?? ''),
-            subtitle: Text(country.capital ?? ''),
-            trailing: country.flag == null
-                ? null
-                : Image.network(
-                    country.flag!,
-                    errorBuilder: (context, error, stackTrace) {
-                      return const Icon(Icons.error, color: Colors.red);
-                    },
-                  ),
-          );
-        },
-      );
-    }
+  //         return ListTile(
+  //           title: Text(country.name ?? ''),
+  //           subtitle: Text(country.abbreviation ?? ''),
+  //           trailing: country.flag == null
+  //               ? null
+  //               : Image.network(
+  //                   country.flag!,
+  //                   errorBuilder: (context, error, stackTrace) {
+  //                     return const Icon(Icons.error, color: Colors.red);
+  //                   },
+  //                 ),
+  //         );
+  //       },
+  //     );
+  //   }
+  // }
+
+  Future<void> _showCountryDetails(String capital, int population,
+      String currency, String phone, String flag, String emblem) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Country Details'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('Capital: $capital'),
+                Text('Population: $population'),
+                Text('Currency: $currency'),
+                Text('Phone: $phone'),
+                flag.isEmpty
+                    ? Image.network(
+                        flag,
+                        errorBuilder: (context, error, stackTrace) {
+                          return const Text('Image not available');
+                        },
+                      )
+                    : const Text('No image available'),
+                emblem.isEmpty
+                    ? Image.network(
+                        emblem,
+                        errorBuilder: (context, error, stackTrace) {
+                          return const Text('Image not available');
+                        },
+                      )
+                    : const Text('No image available'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('CLOSE'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 }
